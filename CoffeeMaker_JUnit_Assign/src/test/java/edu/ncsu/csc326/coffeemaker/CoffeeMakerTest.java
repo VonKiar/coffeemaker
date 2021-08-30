53c27f02b88e8348c8edd3b7f75ca0ba1956a3ea
Potchara <potchara.j@ku.th>
2021-08-30 23:12:41 +0700

    test added

---
+++ CoffeeMaker_JUnit_Assign/src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2009,  Sarah Heckman, Laurie Williams, Dright Ho
+ * All Rights Reserved.
+ *
+ * Permission has been explicitly granted to the University of Minnesota
+ * Software Engineering Center to use and distribute this source for
+ * educational purposes, including delivering online education through
+ * Coursera or other entities.
+ *
+ * No warranty is given regarding this software, including warranties as
+ * to the correctness or completeness of this software, including
+ * fitness for purpose.
+ *
+ *
+ * Modifications
+ * 20171114 - Ian De Silva - Updated to comply with JUnit 4 and to adhere to
+ * 							 coding standards.  Added test documentation.
+ */
+package edu.ncsu.csc326.coffeemaker;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
+import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
+
+/**
+ * Unit tests for CoffeeMaker class.
+ *
+ * @author Sarah Heckman
+ */
+public class CoffeeMakerTest {
+
+	/**
+	 * The object under test.
+	 */
+	private CoffeeMaker coffeeMaker;
+
+	// Sample recipes to use in testing.
+	private Recipe recipe1;
+	private Recipe recipe2;
+	private Recipe recipe3;
+	private Recipe recipe4;
+
+	/**
+	 * Initializes some recipes to test with and the {@link CoffeeMaker}
+	 * object we wish to test.
+	 *
+	 * @throws RecipeException  if there was an error parsing the ingredient
+	 * 		amount when setting up the recipe.
+	 */
+	@Before
+	public void setUp() throws RecipeException {
+		coffeeMaker = new CoffeeMaker();
+
+		//Set up for r1
+		recipe1 = new Recipe();
+		recipe1.setName("Coffee");
+		recipe1.setAmtChocolate("0");
+		recipe1.setAmtCoffee("3");
+		recipe1.setAmtMilk("1");
+		recipe1.setAmtSugar("1");
+		recipe1.setPrice("50");
+
+		//Set up for r2
+		recipe2 = new Recipe();
+		recipe2.setName("Mocha");
+		recipe2.setAmtChocolate("20");
+		recipe2.setAmtCoffee("3");
+		recipe2.setAmtMilk("1");
+		recipe2.setAmtSugar("1");
+		recipe2.setPrice("75");
+
+		//Set up for r3
+		recipe3 = new Recipe();
+		recipe3.setName("Latte");
+		recipe3.setAmtChocolate("0");
+		recipe3.setAmtCoffee("3");
+		recipe3.setAmtMilk("3");
+		recipe3.setAmtSugar("1");
+		recipe3.setPrice("100");
+
+		//Set up for r4
+		recipe4 = new Recipe();
+		recipe4.setName("Hot Chocolate");
+		recipe4.setAmtChocolate("4");
+		recipe4.setAmtCoffee("0");
+		recipe4.setAmtMilk("1");
+		recipe4.setAmtSugar("1");
+		recipe4.setPrice("65");
+	}
+
+
+	/**
+	 * Given a coffee maker with the default inventory
+	 * When we add inventory with well-formed quantities
+	 * Then we do not get an exception trying to read the inventory quantities.
+	 *
+	 * @throws InventoryException  if there was an error parsing the quanity
+	 * 		to a positive integer.
+	 */
+	@Test
+	public void testAddInventory() throws InventoryException {
+		coffeeMaker.addInventory("4","7","0","9");
+	}
+
+	/**
+	 * Given a coffee maker with the default inventory
+	 * When we add inventory with malformed quantities (i.e., a negative
+	 * quantity and a non-numeric string)
+	 * Then we get an inventory exception
+	 *
+	 * @throws InventoryException  if there was an error parsing the quanity
+	 * 		to a positive integer.
+	 */
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException() throws InventoryException {
+		coffeeMaker.addInventory("4", "-1", "asdf", "3");
+	}
+
+	/**
+	 * Given a coffee maker with one valid recipe
+	 * When we make coffee, selecting the valid recipe and paying more than
+	 * the coffee costs
+	 * Then we get the correct change back.
+	 */
+	@Test
+	public void testMakeCoffee() {
+		coffeeMaker.addRecipe(recipe1);
+		assertEquals(25, coffeeMaker.makeCoffee(0, 75));
+	}
+
+	/**
+	 * Testing add 4th recipe to the recipes.
+	 */
+	@Test
+	public void testAddRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		coffeeMaker.addRecipe(recipe2);
+		coffeeMaker.addRecipe(recipe3);
+		coffeeMaker.addRecipe(recipe4); // This should not be added.
+		assertEquals(3, coffeeMaker.getRecipes().length);
+	}
+
+	/**
+	 * Test delete recipes.
+	 */
+	@Test
+	public void testDeleteRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		assertEquals(recipe1, coffeeMaker.getRecipes()[0]); // Check recipe1.
+		coffeeMaker.deleteRecipe(0); // Delete recipe1.
+		assertNotEquals(recipe1, coffeeMaker.getRecipes()[0]); // Check again.
+	}
+
+	/**
+	 * Testing editing recipes, throws exceptions when error occurs.
+	 */
+	@Test
+	public void testEditRecipe() throws RecipeException {
+		coffeeMaker.addRecipe(recipe1); // Initial object.
+
+		Recipe edited = new Recipe(); // Testing object for editing recipe.
+		edited.setName("Edited");
+		edited.setAmtChocolate("1");
+		edited.setAmtCoffee("2");
+
+		assertEquals(coffeeMaker.getRecipes()[0], recipe1); // Verifying initial object.
+
+		coffeeMaker.editRecipe(0, edited); // Editing.
+
+		// Verifying edited object.
+		assertEquals(coffeeMaker.getRecipes()[0].getAmtCoffee(), edited.getAmtCoffee());
+		assertEquals(coffeeMaker.getRecipes()[0], edited);
+	}
+
+	/**
+	 * Testing purchases of coffee, check changes.
+	 */
+	@Test
+	public void testPurchaseBeverage() {
+		coffeeMaker.addRecipe(recipe1);
+		assertEquals(50, coffeeMaker.makeCoffee(0, 100));
+	}
+}
